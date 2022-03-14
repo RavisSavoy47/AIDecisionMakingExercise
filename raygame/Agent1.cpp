703aeb2e9e3f55ed234fad6a57edcdf613ddecc8
Devin <devin.ray.broussard@gmail.com>
2022-03-14 12:27:42 -0500

    Update Agent1.cpp

--- raygame/Agent1.cpp
+++ raygame/Agent1.cpp
@@ -6,6 +6,7 @@
 #include "Transform2D.h"
 #include "AgentOneStateComponent.h"
 #include "ShootingComponent.h"
+#include "WanderBehavior.h"
 
 Agent1::Agent1(float x, float y, const char* name, float maxForce, float maxSpeed, float health) : Character(x, y, name, maxForce, maxSpeed, health)
 {
@@ -21,14 +22,18 @@ void Agent1::start()
 {
 	Character::start(); //Calls the base character's start function
 
-	//Creates a new seekBehavior and seeks the ballPosition actor
-	SeekBehavior* seekBehavior = new SeekBehavior(nullptr, 200);
-	addComponent(seekBehavior);
-	onAddComponent(seekBehavior);
+	////Creates a new seekBehavior and seeks the ballPosition actor
+	//SeekBehavior* seekBehavior = new SeekBehavior(nullptr, 00);
+	//addComponent(seekBehavior);
+	//onAddComponent(seekBehavior);
 
-	//Component that handles the agent's game states
-	AgentOneStateComponent* stateComponent = new AgentOneStateComponent();
-	addComponent(stateComponent);
+	WanderBehavior* wanderBehavior = new WanderBehavior(10, 100, 100);
+	addComponent(wanderBehavior);
+	onAddComponent(wanderBehavior);
+
+	////Component that handles the agent's game states
+	//AgentOneStateComponent* stateComponent = new AgentOneStateComponent();
+	//addComponent(stateComponent);
 	
 	ShootingComponent* shootingComponent = new ShootingComponent(GameManager::getInstance()->getAgent2(), 1);
 	addComponent(shootingComponent);
